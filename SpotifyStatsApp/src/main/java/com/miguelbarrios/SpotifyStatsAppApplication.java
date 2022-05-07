ec5ee86de41b1abf11756be35d1c33faac94d687
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-06 21:32:57 -0500

    initial config

---
+++ SpotifyStatsApp/src/main/java/com/miguelbarrios/SpotifyStatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.miguelbarrios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyStatsAppApplication.class, args);
+	}
+
+}
---
+++ SpotifyStatsApp/src/test/java/com/miguelbarrios/SpotifyStatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelbarrios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyStatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
